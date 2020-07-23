2af4ea6d629561d4b106179e5fd2608bbe0e5649
Xuzhou Qin <xuzhou.qin@jcdecaux.com>
2020-07-23 16:39:00 +0200

    fix: datetime suite test

--- src/test/scala/dev/qinx/faker/provider/datetime/DatetimeSuite.scala
+++ src/test/scala/dev/qinx/faker/provider/datetime/DatetimeSuite.scala
@@ -6,12 +6,13 @@ import org.scalatest.funsuite.AnyFunSuite
 class DatetimeSuite extends AnyFunSuite {
 
   test("Classic faker API") {
-
+    assert(Faker.datetime().localDate() != null)
+    assert(Faker.datetime().localDateTime() != null)
+    assert(Faker.datetime().localTime() != null)
     println(Faker.datetime().localDate())
     println(Faker.datetime().localDate())
     println(Faker.datetime().localDateTime())
     println(Faker.datetime().localTime())
-
   }
 
 }
